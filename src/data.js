06fbd64ef5b4a7900e0e0e8d14d2841e51e2abeb
Denis Semenov <denissemenov@Deniss-Air.attlocal.net>
2023-02-01 15:28:17 -0800

    first commit

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const data = [
         id: 1,
         name: "Happy Life",
         searchTerm: "Best Seller",
-        price: 66,
+        price: "$66",
         image: "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FrZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -12,7 +12,7 @@ export const data = [
         id: 2,
         name: "Pink Love",
         searchTerm: "Birthday",
-        price: 50,
+        price: "$50",
         image: "https://images.unsplash.com/photo-1621303837174-89787a7d4729?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FrZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -20,7 +20,7 @@ export const data = [
         id:3,
         name: "Tenderness",
         searchTerm: "Wedding",
-        price: 108,
+        price: "$108",
         image: "https://images.unsplash.com/photo-1623428454614-abaf00244e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -28,7 +28,7 @@ export const data = [
         id: 4,
         name: "Sweet Hazelnut",
         searchTerm: "Cupcakes",
-        price: 15,
+        price: "$15",
         image: "https://images.unsplash.com/photo-1603532648955-039310d9ed75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -36,7 +36,7 @@ export const data = [
         id: 5,
         name: "White Queen",
         searchTerm: "Best Seller",
-        price: 45,
+        price: "$45",
         image: "https://images.unsplash.com/photo-1595272568891-123402d0fb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -44,7 +44,7 @@ export const data = [
         id: 6,
         name: "Shiny figs",
         searchTerm: "Wedding",
-        price: 100,
+        price: "$100",
         image: "https://images.unsplash.com/photo-1565661834013-d196ca46e14e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -52,7 +52,7 @@ export const data = [
         id: 7,
         name: "Explosion",
         searchTerm: "Other bakery",
-        price: 13,
+        price: "$13 for 5 item",
         image: "https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -60,7 +60,7 @@ export const data = [
     id: 8,
     name: "Marshmallow Cloud",
     searchTerm: "Other bakery",
-    price: 12,
+    price: "$12 for 5 item",
     image: "https://images.unsplash.com/photo-1568387188834-ff83d00b9915?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -68,7 +68,7 @@ export const data = [
         id: 9,
         name: "Delight",
         searchTerm: "Birthday",
-        price: 57,
+        price: "$57",
         image: "https://images.unsplash.com/photo-1577998474517-7eeeed4e448a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -76,7 +76,7 @@ export const data = [
         id: 10,
         name: "I love you",
         searchTerm: "Wedding",
-        price: 99,
+        price: "$99",
         image: "https://images.unsplash.com/photo-1542007920-992d2c424d09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -84,7 +84,7 @@ export const data = [
         id: 11,
         name: "Chocolate nest",
         searchTerm: "Other bakery",
-        price: 16,
+        price: "$16",
         image: "https://images.unsplash.com/photo-1621417809801-2fba61eaa511?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -92,7 +92,7 @@ export const data = [
         id: 12,
         name: "Sweet Strawberry",
         searchTerm: "Cupcakes",
-        price: 14,
+        price: "$14",
         image: "https://images.unsplash.com/photo-1563778084459-859099e48677?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -100,7 +100,7 @@ export const data = [
         id: 13,
         name: "Light Bunny",
         searchTerm: "Birthday",
-        price: 60,
+        price: "$60",
         image: "https://images.unsplash.com/photo-1618451615316-5d492a2323a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -108,7 +108,7 @@ export const data = [
         id: 14,
         name: "Chef's cake",
         searchTerm: "Best Seller",
-        price: 53,
+        price: "$53",
         image: "https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -116,7 +116,7 @@ export const data = [
         id: 15,
         name: "Beautiful Lilac",
         searchTerm: "Best Seller",
-        price: 50,
+        price: "$50",
         image: "https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -124,7 +124,7 @@ export const data = [
         id: 16,
         name: "Rainbow",
         searchTerm: "Other bakery",
-        price: 13,
+        price: "$13 for 5 item",
         image: "https://images.unsplash.com/photo-1616968739895-d5e590e1720c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjEyfHxjYWtlJTIwc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -132,7 +132,7 @@ export const data = [
         id: 17,
         name: "Red heart",
         searchTerm: "Cupcakes",
-        price: 14,
+        price: "$14",
         image: "https://images.unsplash.com/photo-1614707269211-474b2510b3ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fGN1cGNha2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -140,7 +140,7 @@ export const data = [
         id: 18,
         name: "Easter",
         searchTerm: "Cupcakes",
-        price: 14,
+        price: "$14",
         image: "https://images.unsplash.com/photo-1521886580989-e18b74350ba4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fGN1cGNha2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -148,7 +148,7 @@ export const data = [
         id: 19,
         name: "Flight of fancy",
         searchTerm: "Wedding",
-        price: 103,
+        price: "$103",
         image: "https://images.unsplash.com/photo-1622063531136-e7efff4ece72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHdlZGRpbmclMjBjYWtlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
     },
 
@@ -156,7 +156,7 @@ export const data = [
         id: 20,
         name: "Chocolate Bliss",
         searchTerm: "Birthday",
-        price: 45,
+        price: "$45",
         image: "https://images.unsplash.com/photo-1562023692-9283c11284bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGNha2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     }   
 ]
